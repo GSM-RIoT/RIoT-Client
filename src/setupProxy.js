efcdd8ba4bbf97ba64703bcca90946b7db68ec1f
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-09-16 17:10:28 +0900

    난생처음보는 CORS 30분만에 클리어

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         "/manual",
         createProxyMiddleware({
-            target: "http://10.120.75.224:3000",
+            target: "http://10.120.75.224:3000/manual",
             changeOrigin: true,
         })
     )
