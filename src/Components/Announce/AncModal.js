887190b26ec2e5ae367b34b202d0987a0616da27
JeongTaehwan <wjdxoghks11222@naver.com>
2021-09-16 11:33:27 +0900

    메뉴얼을 추가할뻔했다?

--- src/Components/Announce/AncModal.js
+++ src/Components/Announce/AncModal.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as I from "../../Assets/index";
 import * as S from "./style";
 import * as G from "../GlobalStyle";
+import Anc from "../../anc";
 
 function AncModal() {
+  const [showAncModal, setShowAncModal] = useState(false);
+
   return (
     <>
-        <S.Announce>
-          <G.TitleWrapper>
-            <G.Title>매장</G.Title><G.Title2>안내</G.Title2>
-          </G.TitleWrapper>
-          <G.Image><I.Announce /></G.Image>
-        </S.Announce>
+      <S.Announce showAncModal={showAncModal} onClick={() => setShowAncModal(true)}>
+        <G.TitleWrapper>
+          <G.Title>매장</G.Title><G.Title2>안내</G.Title2>
+        </G.TitleWrapper>
+        <G.Image><I.Announce /></G.Image>
+      </S.Announce>
+      {showAncModal ?
+        <S.Background>
+          <S.ModalContainer>
+            <S.ModalHeader>
+              <div className="HeaderText">Manual</div>
+              <I.Delete onClick={() => setShowAncModal(false)} className="DeleteButton" />
+            </S.ModalHeader>
+            <S.ModalWrapper>
+              <S.ModalProblem>
+                {Anc.map(Anc => (
+                  <li key={Anc.id} class="ProblemWrapper">
+                    <p className="Problem">{Anc.Problem}</p>  <br />
+                  </li>
+                ))}
+              </S.ModalProblem>
+              <S.ModalHandle>
+                {Anc.map(Anc => (
+                  <li key={Anc.id} class="HandleWrapper">
+                    <p className="Handle">{Anc.Handle}</p>
+                  </li>
+                ))}
+              </S.ModalHandle>
+            </S.ModalWrapper>
+          </S.ModalContainer>
+        </S.Background> : null}
     </>
   )
 }
